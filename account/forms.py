b102f6d48457fe08622ca35075edc7f9e3a02a1f
zsprawl <alizadeh.amin296@gmail.com>
2024-01-06 18:32:08 +0100

    before deployment

---
+++ account/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.models import User
+from courses.models import Course
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email',)
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not macth themselves')
+        return cd['password2']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already registered')
+        return data
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email',)
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.exclude(id=self.instance.id).filter(email=data).exists():
+            raise forms.ValidationError('Email already registered')
+        return data
+
+
+class CourseEnrollForm(forms.Form):
+    course = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.HiddenInput)
--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,23 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'account'
+# app_name = 'account' # seted aside for namespace issues
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')), # seted aside for app_name issues
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
-]
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit_profile, name='edit_profile'),
+    path('enroll-course', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('courses/', views.StudentCourseListView.as_view(), name='student_course_list'),
+    path('course/<pk>/', views.StudentCourseDetailView.as_view(), name='student_course_detail'),
+    path('course/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+    ]
